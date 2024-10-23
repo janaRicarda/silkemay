c6f0561d38e3b72c20ae959350f4615a1b51b3aa
Jan Schwarz <jan-paul@schw-a-rz.de>
2024-10-23 11:35:53 +0200

    sharp&framer-motion dependencies installed; Gallery.js and BigImage.js: staggered animation with framer motion added, image optimization, handleImage logic moved from BigImage.js to Gallery.js

--- Components.js/Gallery.js
+++ Components.js/Gallery.js
@@ -2,90 +2,124 @@ import styled from "styled-components";
 import Image from "next/image";
 import { useState } from "react";
 import BigImage from "./BigImage";
+import { motion } from "framer-motion";
 
-export default function GallerySection({paintings, title}) {
+export default function GallerySection({ paintings, title, id }) {
+  const [show, setShow] = useState(false);
+  const [currentImageIndex, setCurrentImageIndex] = useState();
 
-    const [show, setShow] = useState(false);
-    const [paintingToShow, setPaintingToShow] = useState({});
+  const paintingToShow = paintings[currentImageIndex];
 
-    const paintingsLength = paintings.length;
+  const paintingsLength = paintings.length;
 
-    function handleShow(painting) {
-        if (painting === painting) {
-        setShow(!show);
-        setPaintingToShow(painting);
-        }
-    };
+  function handleShow(index) {
+    setShow(!show);
+    setCurrentImageIndex(index);
+  }
 
-    return(
-        <StyledSection>
-            <StyledH2>{title}</StyledH2>
-            <StyledArticle>
-                {paintings.map((painting, index) => (
-                    <StyledButton key={painting.id} onClick={()=>handleShow(painting)}>
-                        <StyledImage priority src={painting.src} alt={painting.title} width={500} height={500}/>
-                    </StyledButton>
-                ))}
-            </StyledArticle>
-            {show && <BigImage 
-                        handleShow={handleShow} 
-                        length={paintingsLength}
-                        painting={paintingToShow}
-                        paintings={paintings}
-                        />} 
-        </StyledSection>
-    )
+  function handlePainting(mode) {
+    if (mode === "next") {
+      setCurrentImageIndex((prev) => (prev + 1) % paintingsLength);
+    } else if (mode === "previous") {
+      setCurrentImageIndex(
+        (prev) => (prev - 1 + paintingsLength) % paintingsLength
+      );
+    }
+  }
+
+  //* variants for staggered animation of images
+  const article = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: { staggerChildren: 0.2 },
+    },
+  };
+
+  const button = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1 },
+  };
+
+  return (
+    <StyledSection key={id}>
+      <StyledH2>{title}</StyledH2>
+      <StyledArticle variants={article} initial="hidden" animate="show">
+        {paintings.map((painting, index) => (
+          <StyledButton
+            key={painting.id}
+            variants={button}
+            onClick={() => handleShow(index)}>
+            <StyledImage
+              src={painting.src}
+              alt={painting.title}
+              placeholder="blur"
+              sizes="(min-width: 600px) 33vw, (min-width: 800px) 50vw,(min-width: 1000px) 33vw,(min-width: 1500px) 25vw, 50vw"
+            />
+          </StyledButton>
+        ))}
+      </StyledArticle>
+      {show && (
+        <BigImage
+          handleShow={handleShow}
+          handlePainting={handlePainting}
+          paintingToShow={paintingToShow}
+          length={paintingsLength}
+        />
+      )}
+    </StyledSection>
+  );
 }
 
 const StyledSection = styled.section`
-width: 100%;
-height: auto;
-padding: 0 3rem 0 2rem;
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: flex-start;
-margin-bottom: 3rem;
+  width: 100%;
+  height: auto;
+  padding: 0 3rem 0 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  margin-bottom: 3rem;
 `;
 
 const StyledH2 = styled.h2`
-font-weight: 100;
-font-size: 1.5rem;
-margin-bottom: 0.5rem;
+  font-weight: 100;
+  font-size: 1.5rem;
+  margin-bottom: 0.5rem;
 `;
 
-const StyledArticle = styled.article`
-width: 100%;
-display: grid;
-grid-template-columns: auto auto;
-justify-content: space-evenly;
-gap: 1rem 1rem;
-@media (min-width: 600px) {
-grid-template-columns: auto auto auto;
-}
-@media (min-width: 800px) {
-grid-template-columns: auto auto;
-}
-@media (min-width: 1000px) {
-grid-template-columns: auto auto auto;
-}
-@media (min-width: 1500px) {
-grid-template-columns: auto auto auto auto;
-}
+const StyledArticle = styled(motion.article)`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  justify-content: space-evenly;
+  gap: 1rem 1rem;
+  @media (min-width: 600px) {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+  @media (min-width: 800px) {
+    grid-template-columns: 1fr 1fr;
+  }
+  @media (min-width: 1000px) {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+  @media (min-width: 1500px) {
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
 `;
 
-const StyledButton = styled.button`
-display: inline-grid;
-cursor: pointer;
-border-style: none;
-background: transparent;
-padding: 0;
-width: inherit;
-height: inherit;
+const StyledButton = styled(motion.button)`
+  display: inline-grid;
+  cursor: pointer;
+  border-style: none;
+  background: transparent;
+  padding: 0;
+  width: inherit;
+  height: inherit;
 `;
 
 const StyledImage = styled(Image)`
-width: 100%;
-height: auto;
-aspect-ratio: 1 / 1;
+  width: 100%;
+  height: auto;
+  aspect-ratio: 1 / 1;
 `;
--- Components.js/Pagination.js
+++ Components.js/Pagination.js
@@ -3,87 +3,96 @@ import GallerySection from "./Gallery";
 import Back from "../public/arrow-left.svg";
 import Forward from "../public/arrow-right.svg";
 
-export default function PulpPagination({items, currentPage, onPageChange}) {
-    const pages = [1, 2, 3, 4, 5, 6, 7];
+export default function PulpPagination({ items, currentPage, onPageChange }) {
+  const pages = [1, 2, 3, 4, 5, 6, 7];
 
-    function handleBackwards() {
-        onPageChange(currentPage - 1);
-    };
+  function handleBackwards() {
+    onPageChange(currentPage - 1);
+  }
 
-    function handleForwards() {
-        onPageChange(currentPage + 1)
-    };
+  function handleForwards() {
+    onPageChange(currentPage + 1);
+  }
 
-    return(
-        <article>
-            <GallerySection paintings={items}/>
-            <StyledList>
-              <StyledDiv>
-                {currentPage != 1 && <StyledButton onClick={handleBackwards}><StyledBack /></StyledButton>}
-                <p> Seite {currentPage} von 7</p>
-                {currentPage != 7 && <StyledButton onClick={handleForwards}><StyledForward /></StyledButton>}
-                </StyledDiv>
-                <StyledDiv>
-                {pages.map((page) => (
-                    <StyledListItem  $itemActive={page === currentPage} key={page}>
-                        <a onClick={()=>onPageChange(page)}>{page}</a>
-                    </StyledListItem>
-                ))}
-                </StyledDiv>
-            </StyledList>
-        </article>
-    )
+  return (
+    <article>
+      <GallerySection paintings={items} id={currentPage} />
+      <StyledList>
+        <StyledDiv>
+          {currentPage != 1 && (
+            <StyledButton onClick={handleBackwards}>
+              <StyledBack />
+            </StyledButton>
+          )}
+          <p> Seite {currentPage} von 7</p>
+          {currentPage != 7 && (
+            <StyledButton onClick={handleForwards}>
+              <StyledForward />
+            </StyledButton>
+          )}
+        </StyledDiv>
+        <StyledDiv>
+          {pages.map((page) => (
+            <StyledListItem $itemActive={page === currentPage} key={page}>
+              <a onClick={() => onPageChange(page)}>{page}</a>
+            </StyledListItem>
+          ))}
+        </StyledDiv>
+      </StyledList>
+    </article>
+  );
 }
 
 const StyledList = styled.ul`
- display: flex;
- flex-direction: column;
- justify-content: center;
- align-items: center;
- gap: 0.8rem;
- list-style: none;
- padding: 0;
- margin-bottom: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0.8rem;
+  list-style: none;
+  padding: 0;
+  margin-bottom: 2rem;
 `;
 
 const StyledDiv = styled.div`
-display: flex;
- justify-content: center;
- align-items: center;
- gap: 0.8rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.8rem;
 `;
 
 const StyledListItem = styled.li`
-background: ${({$itemActive}) => ($itemActive ? "var(--dark-font)" : "var(--link)")};
-display: flex;
- justify-content: center;
- align-items: center;
- width: ${({$itemActive}) => ($itemActive ? "1.2rem" : "0.8rem")};
- height: ${({$itemActive}) => ($itemActive ? "1.2rem" : "0.8rem")};
- border-radius: 50%;
- cursor: ${({$itemActive}) => ($itemActive ? "none" : "pointer")};
- color: transparent;
- &:hover {
- background: ${({$itemActive}) => ($itemActive ? "var(--dark-font)" : "var(--background)")};
- }
+  background: ${({ $itemActive }) =>
+    $itemActive ? "var(--dark-font)" : "var(--link)"};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: ${({ $itemActive }) => ($itemActive ? "1.2rem" : "0.8rem")};
+  height: ${({ $itemActive }) => ($itemActive ? "1.2rem" : "0.8rem")};
+  border-radius: 50%;
+  cursor: ${({ $itemActive }) => ($itemActive ? "none" : "pointer")};
+  color: transparent;
+  &:hover {
+    background: ${({ $itemActive }) =>
+      $itemActive ? "var(--dark-font)" : "var(--background)"};
+  }
 `;
 
 const StyledButton = styled.button`
-border-style: none;
-background: transparent;
-cursor: pointer;
-&:hover {
-transform: scale(1.5);
-}
+  border-style: none;
+  background: transparent;
+  cursor: pointer;
+  &:hover {
+    transform: scale(1.5);
+  }
 `;
 
 const StyledBack = styled(Back)`
-width: 2rem;
-height: 2rem;
-
+  width: 2rem;
+  height: 2rem;
 `;
 
 const StyledForward = styled(Forward)`
-width: 2rem;
-height: 2rem;
+  width: 2rem;
+  height: 2rem;
 `;
