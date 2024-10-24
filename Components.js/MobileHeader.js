a59843d1fa03d3e0ff147149dee266efcca0c135
Jan Schwarz <jan-paul@schw-a-rz.de>
2024-10-24 09:13:25 +0200

    MobileHeader.js: conditional of whole component with useClientWidth added

--- Components.js/MobileHeader.js
+++ Components.js/MobileHeader.js
@@ -3,51 +3,57 @@ import MobileMenu from "./MobileMenu";
 import { useState } from "react";
 import Menu from "../public/menu.svg";
 import Link from "next/link";
+import useClientWidth from "@/hooks/useClientWidth";
 
 export default function Header() {
-    const [showMenu, setShowMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
-    function handleMenu() {
-        setShowMenu(!showMenu);
-    }
+  function handleMenu() {
+    setShowMenu(!showMenu);
+  }
 
-return(
-    <StyledHeader>
-        <StyledLink href="/">SILKE MAY</StyledLink>
-        <StyledButton onClick={handleMenu}><StyledMenu /></StyledButton>
-        {showMenu && <MobileMenu handleMenu={handleMenu}/>}
+  const isTablet = useClientWidth(">", 800);
+
+  return (
+    <>
+      {isTablet && (
+        <StyledHeader>
+          <StyledLink href="/">SILKE MAY</StyledLink>
+          <StyledButton onClick={handleMenu}>
+            <StyledMenu />
+          </StyledButton>
+          {showMenu && <MobileMenu handleMenu={handleMenu} />}
         </StyledHeader>
-);
+      )}
+    </>
+  );
 }
 
 const StyledHeader = styled.header`
-position: sticky;
-top: 0;
-width: 100%;
-height: auto;
-display: flex;
-justify-content: space-between;
-align-items: center;
-padding: 1rem 2rem 0.5rem 2rem;
-background: white;
-@media(min-width: 800px) {
-display: none;
-}
+  position: sticky;
+  top: 0;
+  width: 100%;
+  height: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 2rem 0.5rem 2rem;
+  background: white;
 `;
 
 const StyledLink = styled(Link)`
-text-decoration: none;
-color: var(--dar-font);
-font-size: 2rem;
+  text-decoration: none;
+  color: var(--dar-font);
+  font-size: 2rem;
 `;
 
 const StyledButton = styled.button`
-border-style: none;
-background: transparent;
-padding: 0;
+  border-style: none;
+  background: transparent;
+  padding: 0;
 `;
 
 const StyledMenu = styled(Menu)`
-width: 3.5rem;
-height: 3.5rem;
+  width: 3.5rem;
+  height: 3.5rem;
 `;
